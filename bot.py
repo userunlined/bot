5e1171b06af6d0b3e44eb616a4dc982868429017
userunlined <pietrohenrydasilvabraga666@gmail.com>
2026-02-05 15:03:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} online!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(os.getenv('DISCORD_TOKEN'))
